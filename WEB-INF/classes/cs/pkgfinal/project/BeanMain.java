6378e0391b4e6b51dc575dc080220b33dc681460
Dilshan Jayaweera <dilaaaajaya@hotmail.com>
2016-07-01 19:08:48 +0530

    Store the downloaded tsv file path and no of projects for single student values in a java bean

--- WEB-INF/classes/cs/pkgfinal/project/BeanMain.java
+++ WEB-INF/classes/cs/pkgfinal/project/BeanMain.java
@@ -4,5 +4,22 @@ import java.util.Arrays;
 import cs.pkgfinal.project.lib.*;
 
 public class BeanMain implements java.io.Serializable {
+	String currentUserFilePath = null;
+	int noOfProjectsToOneStudent = 0;
 
+	public void setCurrentUserFilePath(String currentUserFilePath){
+		this.currentUserFilePath = currentUserFilePath;
+	}
+
+	public String getCurrentUserFilePath(){
+		return this.currentUserFilePath;
+	}
+
+	public void setNoOfProjectsToOneStudent(int noOfProjectsToOneStudent){
+		this.noOfProjectsToOneStudent = noOfProjectsToOneStudent;
+	}
+
+	public int getNoOfProjectsToOneStudent(){
+		return this.noOfProjectsToOneStudent;
+	}
 }
