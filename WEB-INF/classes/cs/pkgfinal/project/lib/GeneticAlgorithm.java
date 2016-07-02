045ed5bf22c57c9f197aa49f92fff595aafd55de
Dilshan Jayaweera <dilaaaajaya@hotmail.com>
2016-07-02 18:35:07 +0530

    Modified according to GIT master branch

--- WEB-INF/classes/cs/pkgfinal/project/lib/GeneticAlgorithm.java
+++ WEB-INF/classes/cs/pkgfinal/project/lib/GeneticAlgorithm.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 
 /*http://www.theprojectspot.com/tutorial-post/creating-a-genetic-algorithm-for-beginners/3
@@ -68,8 +69,7 @@ public class GeneticAlgorithm {
 
     
     public CandidateSolution populate(int noOfPrefs){
-            
-          for(int i=0;i<this.initialPopulation;i++){
+      for(int i=0;i<this.initialPopulation;i++){
               
               CandidateSolution candidateSolution = new CandidateSolution(this.preferenceTable, noOfPrefs);
              
@@ -77,25 +77,40 @@ public class GeneticAlgorithm {
                
           }
           
+         
+          
+         for(int g=0;g<this.generations;g++){
+           
+                     // genarations loop
+          
          Collections.sort(newPopulation); // sort by fitness best fitness to worst fitness
          
-         if ((this.initialPopulation & 1) != 0) {
+         if ((this.newPopulation.size() & 1) != 0) {
                 
              // odd number of parents so remove the parent with smallerst fitness value
              this.newPopulation.remove( this.newPopulation.size() - 1);
          }
          
-         
-       
-         
-         CandidateSolution c1 =  newPopulation.get(0);
-         CandidateSolution c2 = newPopulation.get(1);
-          
-         int crossOverPoint = c1.getCandidateAssignments().size() / 2;
-         
         
+         int crossOverPoint = (newPopulation.get(0).getCandidateAssignments().size() / (new Random().nextInt((newPopulation.get(0).getCandidateAssignments().size() / 2) - 2 + 1) + 2)); // genarate a random crossover point between 2 to size/2
+     
+         int end = 0;
          
-         /* C1 Fucks C2 */
+         
+          List<CandidateSolution> tempNewPopulation = new ArrayList<CandidateSolution>(); // genaration holder
+      
+      
+         for(int i=0;i<this.newPopulation.size() / 2;i++){
+             
+             int parentA  = end;
+             int parentB = end+1;
+             
+             CandidateSolution c1 =  newPopulation.get(parentA);
+             CandidateSolution c2 = newPopulation.get(parentB);
+         
+          
+             
+         /* C1 crossover  C2 */
          CandidateSolution newC1 = new CandidateSolution();
          
            for (CandidateAssignment assignment: c1.getCandidateAssignments().subList(0, crossOverPoint)) {
@@ -104,7 +119,7 @@ public class GeneticAlgorithm {
                
                
            }
-            for (CandidateAssignment assignment: c2.getCandidateAssignments().subList(crossOverPoint, 51)) {
+            for (CandidateAssignment assignment: c2.getCandidateAssignments().subList(crossOverPoint, c1.getCandidateAssignments().size())) {
                
                newC1.getCandidateAssignments().add(assignment);
                
@@ -113,14 +128,14 @@ public class GeneticAlgorithm {
            
             newC1.putRepeatedOnHash();
           
-            /* C1 Fucks C2 */
+            /* C1 crossover  C2 */
+           
+             
             
-            
-            
-            /* C2 Fucks C1 */
+         /* C2 Crossover C1 */
          CandidateSolution newC2 = new CandidateSolution();
          
-           for (CandidateAssignment assignment: c1.getCandidateAssignments().subList(crossOverPoint, 51)) {
+           for (CandidateAssignment assignment: c1.getCandidateAssignments().subList(crossOverPoint, c1.getCandidateAssignments().size())) {
                
                newC2.getCandidateAssignments().add(assignment);
                
@@ -134,14 +149,75 @@ public class GeneticAlgorithm {
            }
            
             newC2.putRepeatedOnHash();
-          
-            /* C2 Fucks C1 */
             
+             /* C2 Crossover C1 */
+            
+            /* if goal achieve return it */
+            if(c1.getFitness() == 0){
+                
+                return c1;
+            }
+            if(c2.getFitness() == 0){
+                
+                return c2;
+            }
+            if(newC1.getFitness() == 0){
+                
+                return newC1;
+            }
+            if(newC2.getFitness() == 0){
+                
+                return newC2;
+            }
+              /* if goal achieve return it */
+          
+          
+            
+            
+
+            
+            //only replace parent if child is better  (survival of child)
+            if(c1.getFitness() > newC1.getFitness()){
+                
+                 tempNewPopulation.add(c1);
+                 
+            }else{
+                
+                  tempNewPopulation.add(newC1);
+                
+            }
+            if(c2.getFitness() > newC2.getFitness()){
+                
+                 tempNewPopulation.add(c2);
+            }else{
+                
+                  tempNewPopulation.add(newC2);
+            }
+             //only replace parent if child is better (survival of child)
+            
+          
+          
+            
+                   
+             end = (parentB)+1;
+             
+             
+             
+         }
+       
+         
+         this.newPopulation.clear();
+         this.newPopulation = tempNewPopulation; // set current genaration to newPopulation
+         
+        
+         
+      
+         }
+         
+         
+      
            
-         
- 
-         
-        return null;
+        return this.newPopulation.get(0); // return the best solution with maximum fitness
     }
 
   
