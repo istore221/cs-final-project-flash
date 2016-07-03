162cf381a1ded7a2be96cc756ce4b13ee8a02ad2
Dilshan Jayaweera <dilaaaajaya@hotmail.com>
2016-07-03 14:52:53 +0530

    Modified functions to get solution of SA and GA. 1. now the functions do not return validity of the solution instead it returns the % of the conlicted and non conlicted projects. 2. functions returns the conflicted projects with no. of conflicts. 3. functions returns disappointment of the student.

--- WEB-INF/classes/cs/pkgfinal/project/BeanMain.java
+++ WEB-INF/classes/cs/pkgfinal/project/BeanMain.java
@@ -47,26 +47,6 @@ public class BeanMain implements java.io.Serializable {
 				}
 			}
 		
-			JSONObject summary1 = new JSONObject();
-			summary1.put("title", "Energy");
-			summary1.put("value", solution.getEnergy());
-			
-			JSONObject summary2 = new JSONObject();
-			summary2.put("title", "Time taken to run");
-			summary2.put("value", (Math.round(time_took * 100.0) / 100.0) + " " + time_took_string);
-			
-			JSONObject summary3 = new JSONObject();
-			summary3.put("title", "Validity");
-			summary3.put("value", solution.isValid() ? "Valid" : "Not Valid");
-			
-			JSONArray summary_values = new JSONArray();
-			summary_values.add(summary1);
-			summary_values.add(summary2);
-			summary_values.add(summary3);
-
-			mainJson.put("summary", summary_values);
-			
-			
 			JSONObject tableheader1 = new JSONObject();
 			tableheader1.put("tableheader", "#");
 			
@@ -78,16 +58,20 @@ public class BeanMain implements java.io.Serializable {
 
 			JSONObject tableheader4 = new JSONObject();
 			tableheader4.put("tableheader", "Current Project");
+
+			JSONObject tableheader5 = new JSONObject();
+			tableheader5.put("tableheader", "Disappointed");
 			
 			JSONArray table_headers = new JSONArray();
 			table_headers.add(tableheader1);
 			table_headers.add(tableheader2);
 			table_headers.add(tableheader3);
 			table_headers.add(tableheader4);
+			table_headers.add(tableheader5);
 
 			mainJson.put("tableheaders", table_headers);
 			
-			int row = 1;
+			int row = 1, non_conflict_count = 0;
 			JSONArray table_data = new JSONArray();
 	        for(CandidateAssignment caAss : solution.getCandidateAssignments()){
 				
@@ -113,11 +97,52 @@ public class BeanMain implements java.io.Serializable {
             	}
             	else {
                 	tabledata.put("col5", false);
+                	non_conflict_count++;
             	}
+
+            	tabledata.put("col6", caAss.getDisappointment());
 
 	        	table_data.add(tabledata);
 	        }
 			mainJson.put("tabledata", table_data);
+
+			JSONObject summary1 = new JSONObject();
+			summary1.put("title", "Energy");
+			summary1.put("value", solution.getEnergy() + " (closer to 0 is better)");
+			
+			JSONObject summary2 = new JSONObject();
+			summary2.put("title", "Time taken to run");
+			summary2.put("value", (Math.round(time_took * 100.0) / 100.0) + " " + time_took_string);
+			
+			double nonConflctPrjs = (Math.round((((float)non_conflict_count / (float)solution.getCandidateAssignments().size()) * 100) * 100.0) / 100.0);
+			
+			JSONObject summary3 = new JSONObject();
+			summary3.put("title", "Non conflicted projects");
+			summary3.put("value", nonConflctPrjs + " %");
+			
+			JSONObject summary4 = new JSONObject();
+			summary4.put("title", "Conflicted projects");
+			summary4.put("value", (Math.round((100 - nonConflctPrjs) * 100.0) / 100.0) + " %");
+
+			JSONArray conflicted_projects = new JSONArray();
+			for(String projectName : solution.getDuplicateAssignedProjects().keySet()){
+				JSONObject conflict_project = new JSONObject();
+                conflict_project.put("confl", projectName + " - " + solution.getDuplicateAssignedProjects().get(projectName));
+                conflicted_projects.add(conflict_project);
+			}
+			
+			JSONObject summary5 = new JSONObject();
+			summary5.put("title", "Conflicting projects");
+			summary5.put("value", conflicted_projects);
+			
+			JSONArray summary_values = new JSONArray();
+			summary_values.add(summary1);
+			summary_values.add(summary2);
+			summary_values.add(summary3);
+			summary_values.add(summary4);
+			summary_values.add(summary5);
+
+			mainJson.put("summary", summary_values);
 		}
 		catch (FileNotFoundException ex) {
 	    	return ex.getMessage();        
@@ -152,26 +177,6 @@ public class BeanMain implements java.io.Serializable {
 				}
 			}
 		
-			JSONObject summary1 = new JSONObject();
-			summary1.put("title", "Fitness");
-			summary1.put("value", solution.getFitness());
-			
-			JSONObject summary2 = new JSONObject();
-			summary2.put("title", "Time taken to run");
-			summary2.put("value", (Math.round(time_took * 100.0) / 100.0) + " " + time_took_string);
-			
-			JSONObject summary3 = new JSONObject();
-			summary3.put("title", "Validity");
-			summary3.put("value", solution.isValid() ? "Valid" : "Not Valid");
-			
-			JSONArray summary_values = new JSONArray();
-			summary_values.add(summary1);
-			summary_values.add(summary2);
-			summary_values.add(summary3);
-
-			mainJson.put("summary", summary_values);
-			
-			
 			JSONObject tableheader1 = new JSONObject();
 			tableheader1.put("tableheader", "#");
 			
@@ -183,16 +188,20 @@ public class BeanMain implements java.io.Serializable {
 
 			JSONObject tableheader4 = new JSONObject();
 			tableheader4.put("tableheader", "Current Project");
+
+			JSONObject tableheader5 = new JSONObject();
+			tableheader5.put("tableheader", "Disappointed");
 			
 			JSONArray table_headers = new JSONArray();
 			table_headers.add(tableheader1);
 			table_headers.add(tableheader2);
 			table_headers.add(tableheader3);
 			table_headers.add(tableheader4);
+			table_headers.add(tableheader5);
 
 			mainJson.put("tableheaders", table_headers);
 			
-			int row = 1;
+			int row = 1, non_conflict_count = 0;
 			JSONArray table_data = new JSONArray();
 	        for(CandidateAssignment caAss : solution.getCandidateAssignments()){
 				
@@ -218,11 +227,52 @@ public class BeanMain implements java.io.Serializable {
             	}
             	else {
                 	tabledata.put("col5", false);
+                	non_conflict_count++;
             	}
+
+            	tabledata.put("col6", caAss.getDisappointment());
 
 	        	table_data.add(tabledata);
 	        }
 			mainJson.put("tabledata", table_data);
+
+			JSONObject summary1 = new JSONObject();
+			summary1.put("title", "Fitness");
+			summary1.put("value", solution.getFitness() + " (closer to 0 is better)");
+			
+			JSONObject summary2 = new JSONObject();
+			summary2.put("title", "Time taken to run");
+			summary2.put("value", (Math.round(time_took * 100.0) / 100.0) + " " + time_took_string);
+			
+			double nonConflctPrjs = (Math.round((((float)non_conflict_count / (float)solution.getCandidateAssignments().size()) * 100) * 100.0) / 100.0);
+			
+			JSONObject summary3 = new JSONObject();
+			summary3.put("title", "Non conflicted projects");
+			summary3.put("value", nonConflctPrjs + " %");
+			
+			JSONObject summary4 = new JSONObject();
+			summary4.put("title", "Conflicted projects");
+			summary4.put("value", (Math.round((100 - nonConflctPrjs) * 100.0) / 100.0) + " %");
+
+			JSONArray conflicted_projects = new JSONArray();
+			for(String projectName : solution.getDuplicateAssignedProjects().keySet()){
+				JSONObject conflict_project = new JSONObject();
+                conflict_project.put("confl", projectName + " - " + solution.getDuplicateAssignedProjects().get(projectName));
+                conflicted_projects.add(conflict_project);
+			}
+			
+			JSONObject summary5 = new JSONObject();
+			summary5.put("title", "Conflicting projects");
+			summary5.put("value", conflicted_projects);
+			
+			JSONArray summary_values = new JSONArray();
+			summary_values.add(summary1);
+			summary_values.add(summary2);
+			summary_values.add(summary3);
+			summary_values.add(summary4);
+			summary_values.add(summary5);
+
+			mainJson.put("summary", summary_values);
 		}
 		catch (FileNotFoundException ex) {
 	    	return ex.getMessage();        
